c33c7e22ec66ddb3d2a799333c793aaf7b65d67c
jcreed2 <jcreed2@ncsu.edu>
2019-11-26 18:30:02 -0500

    things for my home compiter jr

---
+++ plotThr.m
@@ -0,0 +1,17 @@
+parseLogsout
+ figure
+for i = 1:100:length(tsc.kitePosGnd.time)
+   
+   
+    scatter3(tsc.kitePosGnd.data(i,1),tsc.kitePosGnd.data(i,2),tsc.kitePosGnd.data(i,3))
+     xlim([0 1000])
+      zlim([0 1000])
+       ylim([-300 300])
+    hold on 
+    scatter3(tsc.node2Pos.data(i,1),tsc.node2Pos.data(i,2),tsc.node2Pos.data(i,3))
+    scatter3(tsc.node3Pos.data(i,1),tsc.node3Pos.data(i,2),tsc.node3Pos.data(i,3))
+    scatter3(tsc.node4Pos.data(i,1),tsc.node4Pos.data(i,2),tsc.node4Pos.data(i,3))
+    scatter3(tsc.node5Pos.data(i,1),tsc.node5Pos.data(i,2),tsc.node5Pos.data(i,3))
+    hold off
+    pause(.1)
+end
